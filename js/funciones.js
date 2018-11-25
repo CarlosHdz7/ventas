90b247bea3465217d7a979f9253bc8ba0e4af6ea
CarlosHdz7 <carloshdz.udb@gmail.com>
2018-11-25 17:06:47 -0600

    Initial commit

---
+++ js/funciones.js
@@ -0,0 +1,26 @@
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 100) {
+      $('.logo').height(50);
+      $('.logo').width(50);
+
+    }
+    else {
+      $('.logo').height(100);
+      $('.logo').width(100);
+    }
+  }
+);
+
+function validarFormVacio(formulario){
+	datos=$('#' + formulario).serialize();
+	d=datos.split('&');
+	vacios=0;
+	for(i=0;i< d.length;i++){
+			controles=d[i].split("=");
+			if(controles[1]=="A" || controles[1]==""){
+				vacios++;
+			}
+	}
+	return vacios;
+}
